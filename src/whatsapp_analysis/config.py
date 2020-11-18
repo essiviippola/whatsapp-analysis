70cf8de40599cb2a3f1eff84fdc5fd60fc119ab0
Essi Viippola <essi.viippola@live.fi>
2020-11-18 19:02:53 +0200

    Added helper functions

--- src/whatsapp_analysis/config.py
+++ src/whatsapp_analysis/config.py
@@ -1,5 +1,13 @@
 from pathlib import Path
 import numpy
 import pandas as pd
+import regex
+import emoji
+import plotly.express as px
+import nltk
+from wordcloud import WordCloud
+from matplotlib import pyplot as plt
+from collections import Counter
+from nltk.corpus import stopwords
 
-data_dir = Path('../data')
+data_path = Path('../data/whatsapp.txt')
